bf168c3c5c7894369c0307376723d973166f6118
Victor Milovanov <lostfreeman@gmail.com>
2014-02-12 05:50:57 -0800

    + translator stub

---
+++ CoreLR.Translation/ModuleTranslationOptions.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace CoreLR.Translation
+{
+	public sealed class ModuleTranslationOptions
+	{
+		public ModuleTranslationOptions(string inputPath)
+		{
+			InputPath = inputPath;
+			OutputPath = Path.ChangeExtension(inputPath, ".elf");
+		}
+
+		public string InputPath { get; set; }
+		public string OutputPath { get; set; }
+
+		public string ModuleName
+		{
+			get { return Path.GetFileNameWithoutExtension(OutputPath); }
+		}
+	}
+}
---
+++ CoreLR.Translation/ModuleTranslator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using LLVM;
+using Mono.Cecil;
+
+namespace CoreLR.Translation
+{
+	public sealed class ModuleTranslator
+	{
+		public ModuleTranslator(ModuleTranslationOptions options)
+		{
+			resolver = new DefaultAssemblyResolver();
+			context = Context.Global;
+			module = new Module(options.ModuleName, context);
+			assembly = resolver.Resolve(options.InputPath);
+			this.options = options;
+		}
+
+		readonly ModuleTranslationOptions options;
+		readonly BaseAssemblyResolver resolver;
+		readonly Context context;
+		readonly Module module;
+		readonly AssemblyDefinition assembly;
+
+		public void Translate()
+		{
+			foreach (var moduleDefinition in assembly.Modules)
+			{
+				foreach (var typeDefinition in moduleDefinition.Types)
+				{
+					Translate(typeDefinition);
+				}
+			}
+		}
+
+		void Translate(TypeDefinition typeDefinition)
+		{
+			TranslateStaticFields(typeDefinition);
+
+			foreach (var methodDefinition in typeDefinition.Methods)
+			{
+				Translate(methodDefinition);
+			}
+		}
+
+		void Translate(MethodDefinition methodDefinition)
+		{
+			throw new NotImplementedException();
+		}
+
+		void TranslateStaticFields(TypeDefinition typeDefinition)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

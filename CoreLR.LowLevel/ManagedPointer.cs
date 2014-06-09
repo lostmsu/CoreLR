c5d9ce700a9a6b4d406b9790685e079055dcef0f
Victor Milovanov <lostfreeman@gmail.com>
2014-06-09 00:16:11 -0700

    + CoreLR.LowLevel

---
+++ CoreLR.LowLevel/ManagedPointer.cs
@@ -0,0 +1,13 @@
+﻿namespace CoreLR.LowLevel
+{
+	using System;
+
+	/// <summary>
+	/// Struct, representing managed pointers (e.g. pointers tracked by GC)
+	/// </summary>
+	/// <typeparam name="T">Type of the item, referenced by this pointer</typeparam>
+	public struct ManagedPointer<T> where T : struct
+	{
+		IntPtr pointer;
+	}
+}
